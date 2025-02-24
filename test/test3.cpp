5a45feee13a9aa5b95dca5140e62e2971c75714f
xumingjie <1044011439@qq.com>
2025-02-24 22:01:00 +0800

    并发计算和单计算的比较

--- test/test3.cpp
+++ test/test3.cpp
@@ -20,6 +20,20 @@ class MyTask:public Task{
             return sum;
         }
 };
+class MyTask2:public Task{
+    private:
+    public:
+        MyTask2() = default;
+        ~MyTask2() = default;
+        MyAny run(){
+            ULong sum = 0;
+            for(int i = 1;i<=300000000;i++){
+                sum += i;
+            }
+            std::cout<<"sum_ = "<<sum<<std::endl;
+            return sum;
+        }
+};
 
 int main(){
     ThreadPool pool;
@@ -28,15 +42,15 @@ int main(){
     Result res1 = pool.submitTask(std::make_shared<MyTask>(1,100000000));
     Result res2 = pool.submitTask(std::make_shared<MyTask>(100000001,200000000));
     Result res3 = pool.submitTask(std::make_shared<MyTask>(200000001,300000000));
-
+    Result res4 = pool.submitTask(std::make_shared<MyTask2>());
     ULong sum1 = res1.get().cast_<ULong>();
     ULong sum2 = res2.get().cast_<ULong>();
     ULong sum3 = res3.get().cast_<ULong>();
     std::cout<<"sum = "<<sum1+sum2+sum3<<std::endl;
-    ULong sum = 0;
-    for(ULong i = 1;i<=300000000;i++){
-        sum += i;
-    }
-    std::cout<<"sum = "<<sum<<std::endl; 
+    //ULong sum = 0;
+    // for(ULong i = 1;i<=300000000;i++){
+    //     sum += i;
+    // }
+    // std::cout<<"sum = "<<sum<<std::endl; 
     for(;;){}
 }
